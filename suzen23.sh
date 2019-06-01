ed1d1525995c0dd4b3b3cc55f11b646bc2f9e54a
Legion23451 <50868553+Legion23451@users.noreply.github.com>
2019-06-01 16:12:04 +0300

    Update suzen23.sh

--- suzen23.sh
+++ suzen23.sh
@@ -1,8 +1,8 @@
 #!bin/bash/
-# mv *.log /home/suzen/destination/
-# mv nginx1.back.log nginx1.log.back
-# mv nginx2.back.log nginx2.log.back
-# mv nginx3.back.log nginx3.log.back
-# mv nginx4.back.log nginx4.log.back
-# mv nginx5.back.log nginx5.log.back
+mv *.log /home/suzen/destination/
+mv nginx1.back.log nginx1.log.back
+mv nginx2.back.log nginx2.log.back
+mv nginx3.back.log nginx3.log.back
+mv nginx4.back.log nginx4.log.back
+mv nginx5.back.log nginx5.log.back
 echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
