70ac7c16f95c8449065fc22485c6de161710bf1e
Legion23451 <50868553+Legion23451@users.noreply.github.com>
2019-06-01 16:32:38 +0300

    Update suzen21.sh

--- suzen21.sh
+++ suzen21.sh
@@ -1,5 +1,4 @@
 #!bin/bash/
-# rm [0-99]*.txt
-# rm [a-z]*.png
-# rm test-[a-zA-Z0-99]*.log
-echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
+rm [0-99]*.txt
+rm [a-z]*.png
+rm test-[a-zA-Z0-99]*.log
